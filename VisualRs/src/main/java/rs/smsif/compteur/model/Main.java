f9a6b6f4eea952f4f4303caf70538dcedbec7249
brunB <benjamin.brun9569@gmail.com>
2020-04-07 20:19:31 +0200

    modification Main

--- VisualRs/src/main/java/rs/smsif/compteur/model/Main.java
+++ VisualRs/src/main/java/rs/smsif/compteur/model/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 
 	public static void main(String[] args)
 	{
+		System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 		launch(args);
 		System.exit(0);
 	}
@@ -60,7 +61,7 @@ public class Main extends Application {
 		}
 		*/
 
-		System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
 
 		try
 		{
