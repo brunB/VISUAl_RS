50d7939a01b11334053a2d1f9051155e0304373b
brunB <benjamin.brun9569@gmail.com>
2020-04-09 12:21:56 +0200

    Modification de la requête de Récupération des comptages ayant la même rubrique de solde

--- VisualRs/src/main/java/rs/smsif/compteur/utils/OutilsComptage.java
+++ VisualRs/src/main/java/rs/smsif/compteur/utils/OutilsComptage.java
@@ -7,22 +7,22 @@ import rs.smsif.compteur.model.Comptage;
 
 /**
  * Cette classe permet d'effectuer des actions sur les rubriques de solde.
- * 
+ *
  * @author gmarco
  *
  */
 public class OutilsComptage {
-	
+
 	/**
 	 * Retourne les rubriques de solde associées à la rubrique centrale.
-	 * 
+	 *
 	 * @param comptage la rubrique de solde centrale.
 	 * @param comptages la liste de toutes les rubriques de solde.
-	 * 
+	 *
 	 * @return les rubriques de solde associées à la rubrique centrale.
 	 */
 	public static List <Comptage> recupererRubriqueSolde(Comptage comptage, List <Comptage> comptages)
-	{			
+	{
 		String version = comptage.getVersion();
 		String medro = comptage.getMedro();
 		String rubriqueSolde = comptage.getRubriqueSolde();
@@ -34,31 +34,32 @@ public class OutilsComptage {
 	   		        									 .filter(item -> item.getMedro().equals(medro))
 	   		        									 .filter(item -> !item.getRubriqueSolde().equals(rubriqueSolde))
 	   		        									 .collect(Collectors.toList());
-				
+
 		return comptagesSelonVersion;
 	}
-	
+
 	/**
 	 * Retourne les comptages ayant la même rubrique de solde que la rubrique centrale.
-	 * 
+	 *
 	 * @param comptage la rubrique de solde centrale.
 	 * @param comptages la liste de toutes les rubriques de solde.
-	 * 
+	 *
 	 * @return les comptages ayant la même rubrique de solde que la rubrique centrale.
 	 */
 	public static List <Comptage> recupererMedros(Comptage comptage, List <Comptage> comptages)
 	{
 		String version = comptage.getVersion();
 		String rubriqueSolde = comptage.getRubriqueSolde();
-		
+
 		// Récupération des comptages ayant la même rubrique de solde.
 		List <Comptage> comptagesSelonMedro = comptages.stream()
-					 								   .filter(item -> item.getVersion().equals(version))
+					 								   .filter(item -> item.getVersion().substring(0, 10).equals(version.substring(0, 10)))
 					 								   .filter(item -> item.getRubriqueSolde().equals(rubriqueSolde))
+					 								   .filter(item -> item.getIndic() == 1)
 					 								   .collect(Collectors.toList());
-		
+
 		comptagesSelonMedro.remove(comptage);
-		
+
 		return comptagesSelonMedro;
 	}
 }
